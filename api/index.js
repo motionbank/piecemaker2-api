7ed846116785735388da3f63f2db2b6d44c0843d
Mattes <mattes005@gmail.com>
2013-01-03 15:27:39 +0100

    express added

--- api/index.js
+++ api/index.js
@@ -1,4 +1,8 @@
 var mysql      = require('mysql');
+var express = require('express');
+var app = express();
+
+
 var connection = mysql.createConnection({
   host     : 'kb-server.de',
   user     : 'd015dedf',
@@ -14,3 +18,7 @@ connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
 });
 
 connection.end();
+
+
+app.listen(8081);
+console.log('Listening on port 8081');
