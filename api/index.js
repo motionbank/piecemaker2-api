874f1a1bec98ed3885a03ee6bad473ac8d52b473
Mattes <mattes005@gmail.com>
2013-01-04 14:00:30 +0100

    db updated

--- api/index.js
+++ api/index.js
@@ -7,21 +7,38 @@ var sequelize = new Sequelize('d015dedf', 'd015dedf', 'QUtNzpy3QF25gv3E', {
   host: 'kb-server.de',
   dialect: 'mysql',
   define: {
+    engine: 'InnoDB',
     charset: 'utf8',
     collate: 'utf8_general_ci',
-    timestamps: false
+    timestamps: false,
+    underscored: true
   }
 });
 
-
+// Define tables
 var User = sequelize.define('user', {
   user_id: {type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true},
   name: {type: Sequelize.STRING},
-  email: {type: Sequelize.STRING}
+  email: {type: Sequelize.STRING},
+  password: {type: Sequelize.STRING},
+  api_access_key: {type: Sequelize.STRING},
+  is_admin: {type: Sequelize.BOOLEAN},
+  is_disabled: {type: Sequelize.BOOLEAN}
 });
+
+
+var Event = sequelize.define('event', {
+  event_id: {type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true},
+  utc_timestamp: {type: Sequelize.STRING},
+  duraction: {type: Sequelize.STRING}
+});
+
+User.hasMany(Event, {foreignKey: 'created_by_user_id'});
+
 
 User.findAll().success(function(users) {
   console.log(users);
+  console.log(users[0].getEvents());
 });
 
 
