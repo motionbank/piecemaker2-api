3c2c1728d742391172db18e4424dcc4261f8839f
mattes <matthias.kadenbach@gmail.com>
2013-01-16 02:59:17 +0100

    error handler working

--- api/config.js
+++ api/config.js
@@ -4,13 +4,15 @@
 
 module.exports = {
 
+  env: "production", // development|production
+
   // see https://github.com/felixge/node-mysql#connection-options for options
   mysql: {
     host: 'kb-server.de',
     database: 'd0161511',
     user: 'd0161511',
     password: 'nmsN8dCS5yB3mFUk',
-    debug: false
+    debug: false // set to false, if config.env = production
   }
 
 };
--- api/index.js
+++ api/index.js
@@ -1,11 +1,11 @@
 var config = require('./config.js');
 var mysql = require('mysql');
-var connection = mysql.createConnection(config.mysql);
-
 var connect = require('connect')
 var http = require('http');
 
 
+if(config.env == 'development' && config.mysql.debug) config.mysql.debug = false;
+var connection = mysql.createConnection(config.mysql);
 
 
 connection.destroy();
@@ -13,34 +13,57 @@ connection.destroy();
 
 
 var app = connect()
-  // .use(connect.cookieParser())
-  // .use(connect.session({ secret: 'my secret here' }))
+// .use(connect.cookieParser())
+// .use(connect.session({ secret: 'my secret here' }))
 
 
-  // API ROUTER MIDDLEWARE
-  // =====================
-  .use(function(req, res, next){
-    
+// API ROUTER MIDDLEWARE
+// =====================
+.use(function(req, res, next){
+  
 
-    // find first a-z and _ in url.
-    // example /controller/:id = controller
-    try {
-      var controllerName = req.url.match(/^\/([a-z_]+)/)[1];
-    } catch(e) {
-      console.log(e);
-    }
-    
-    // load controller and delegate execution
-    // controllerName should be safe here, only contains a-z and _
-    try {
-      require('./controllers/' + controllerName + '.js').call(null, req, res, next);
-    } catch(e) {
-      console.log(e);
-    }
+  // find first a-z and _ in url.
+  // example /controller/:id = controller
+  try {
+    var controllerName = req.url.match(/^\/([a-z_]+)/)[1];
+  } catch(e) {
+    throw new Error('invalid controller name');
+  }
+  
+  // load controller and delegate execution
+  // controllerName should be safe here, only contains a-z and _
+  try {
+    require('./controllers/' + controllerName + '.js').call(null, req, res, next);
+  } catch(e) {
+    throw new Error('controller not found');
+  }
+  
+  res.end('Hello from Connect!\n');
+})
 
-    // res.end('Hello from Connect!\n');
-  });
 
+// ERROR HANDLER MIDDLEWARE
+// ========================
+.use(function(err, req, res, next){
+  // copy & paste with some modifications from http://www.senchalabs.org/connect/errorHandler.html
+  if(err.status) res.statusCode = err.status;
+  if(res.statusCode < 400) res.statusCode = 500;
+  var accept = req.headers.accept || '';
+
+  // handle json
+  if(~accept.indexOf('json')) {
+    var error = {message: err.message, statusCode: res.statusCode};
+    if(config.env == 'development') error.stack = err.stack;
+    for (var prop in err) error[prop] = err[prop];
+    var json = JSON.stringify({ error: error });
+    res.setHeader('Content-Type', 'application/json');
+    res.end(json);
+  // plain text
+  } else {
+    res.writeHead(res.statusCode, { 'Content-Type': 'text/plain' });
+    res.end('(HTTP ' + res.statusCode + ') ' + (config.env == 'development' ? err.stack : err.message));
+  }
+});
 
 
 http.createServer(app).listen(8080, function() {
