2e9a05efa9356bfe3d59087456af71eb16c193a1
mattes <matthias.kadenbach@gmail.com>
2013-01-17 02:05:05 +0100

    routes work

--- api/config.js
+++ api/config.js
@@ -1,11 +1,19 @@
 /*
  * Config File
  */
-
 module.exports = {
 
+  // environment setting
+  // controls logging and so on
   env: "development", // development|production
 
+  // listen on this port
+  port: '8080', 
+
+  // which HTTP methods/verbs do you want to allow? (uppercase!)
+  allowHttpMethods: ['GET', 'POST', 'PUT', 'DEL'],
+
+  // mysql settings
   // see https://github.com/felixge/node-mysql#connection-options for options
   mysql: {
     host: 'kb-server.de',
