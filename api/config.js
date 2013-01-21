674e5f9af6ce845a83528abdd27db2e0729337fc
Mattes <mattes005@gmail.com>
2013-01-21 23:48:51 +0100

    mysql requests for routes

--- api/config.js
+++ api/config.js
@@ -6,12 +6,13 @@ module.exports = {
   // environment setting
   // controls logging and so on
   env: 'development', // development|production
+  // @todo env = process.env.NODE_ENV || 'development';
 
   // listen on this port
   port: '8080',
 
   // which HTTP methods/verbs do you want to allow? (uppercase!)
-  allowHttpMethods: ['GET', 'POST', 'PUT', 'DEL'],
+  allowHttpMethods: ['GET', 'POST', 'PUT', 'DELETE'],
 
   // mysql settings
   // see https://github.com/felixge/node-mysql#connection-options for options
@@ -20,7 +21,7 @@ module.exports = {
     database: 'd0161511',
     user: 'd0161511',
     password: 'nmsN8dCS5yB3mFUk',
-    debug: false // is set to false, if config.env = production
+    debug: true // is set to false, if config.env = production
   }
 
 };
