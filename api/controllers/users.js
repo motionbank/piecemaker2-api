c4e584855e8983b43ac1fddff037c482fa6c9044
mattes <matthias.kadenbach@gmail.com>
2013-01-30 19:03:39 +0100

    controllers updated

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -5,19 +5,12 @@ module.exports = {
   // > curl -X GET http://localhost:8080/users
   //
   // < 200 < json < [{"id": 1, "name": "Peter", "email": "peter@example.com", "is_admin": 0}]
-  // < 401 < json < {"error": 401, "message": "unauthorized"}
+  // < 401 < json < {"http": 401, "error": "unauthorized"}
+  // < 500 < json < {"http": 401, "error": "unauthorized"}
   'GET /users':
   function($) {
-    $.db.query('SELECT * FROM users WHERE is_disabled=0', 
-      function(error, results) {
-        if(error) {
-          return $.error(500, error);
-        } else {
-          return $.render(results);
-        }
-    });
+    $.m.get_all($, 'SELECT * FROM users WHERE is_disabled=0');
   },
-
 
   // > POST /user > json
   // > create new user
@@ -25,21 +18,13 @@ module.exports = {
   // > curl -X POST --data "name=Matthias&email=matthias@example.com&password=12345" http://localhost:8080/user
   // 
   // < 200 < json < {"id": 1}
-  // < 400 < json < {"error": 400, "message": "invalid parameters"}
-  // < 401 < json < {"error": 401, "message": "unauthorized"}
+  // < 500 < json < {"http": 500, "error": "unable to create new item"}
+  // < 401 < json < {"http": 401, "error": "unauthorized"}
   'POST /user':
   function($) {
-    var $AccessKey = 'some_random_string'; // @todo
-    $.db.query('INSERT INTO users SET ' +
-      'name=?, email=?, password=SHA1(?), $_access_key=?',
-      [$.params.name, $.params.email, $.params.password, $AccessKey],
-      function(error, results) {
-        if(error) {
-          return $.error(400, error);
-        } else {
-          return $.render(results.insertId);
-        }
-      });
+    var accessKey = 'some_random_string'; // @todo
+    $.m.post_one($, 'INSERT INTO users SET name=?, email=?, password=SHA1(?), api_access_key=?', 
+      [$.params.name, $.params.email, $.params.password, accessKey]);
   },
 
   // > GET /user/:int > json
@@ -47,75 +32,26 @@ module.exports = {
   // > curl -X GET http://localhost:8080/user/1
   // 
   // < 200 < json < {"id": 1, "name": "Peter", "email": "peter@example.com"}
-  // < 400 < json < {"error": 400, "message": "invalid parameters"} < means user_id from url is missing or was not found
-  // < 401 < json < {"error": 401, "message": "unauthorized"}
+  // < 400 < json < {"http": 400, "error": "invalid parameters"} < means user_id from url is missing or was not found
+  // < 500 < json < {"http": 500, "error": "unable to fetch result"}
+  // < 401 < json < {"http": 401, "error": "unauthorized"}
   'GET /user/:int':
   function($, user_id) {
-    if(!user_id) {
-      return $.error('invalid user_id');
-    }
-
-    $.db.query('SELECT * FROM users WHERE isd=? LIMIT 1', [user_id], 
-      function(error, results) {
-        if(error) {
-          return $.error(400, 'invalid parameters');
-        } else {
-          return $.render(results[0] || 'false'); 
-        }
-    });
+    $.m.get_one($, user_id, 'SELECT id, name, email FROM users WHERE id=? LIMIT 1');
   },
-
 
   // > PUT /user/:int > json
   // > updates a user
   // > {"name": "Peter", "email": "peter@example.com"}
   // > curl -X PUT --data "email=newemail@example.com" http://localhost:8080/user/1
   // 
-  // < 200 < json < {"id": 1, "name": "Peter", "email": "peter@example.com"}
-  // < 400 < json < {"error": 400, "message": "invalid parameters"}
-  // < 401 < json < {"error": 401, "message": "unauthorized"}
+  // < 200 < json < {"id": 1}
+  // < 400 < json < {"http": 400, "error": "invalid parameters"}
+  // < 500 < json < {"http": 500, "error": "unable to update item"}
+  // < 401 < json < {"http": 401, "error": "unauthorized"}
   'PUT /user/:int':
   function($, user_id) {
-    if(!user_id) {
-      return $.error(400, 'invalid user_id');
-    }
-
-    // filter $.params
-    var allowFields = ['name', 'email'];
-    var updateKeys = [];
-    var updateValues = [];
-    var $ParamsKeys = Object.keys($.params);
-    var $ParamsKeysLength = $ParamsKeys.length;
-    for(var i=0; i < $ParamsKeysLength; i++) {
-      if(~allowFields.indexOf($ParamsKeys[i])) {
-        updateKeys.push($ParamsKeys[i] + '=?');
-        updateValues.push($.params[$ParamsKeys[i]]);
-      }
-    }
-
-    // anything to update?
-    if(updateKeys.length == 0) {
-      return $.render('false');
-    }
-
-    updateValues.push(user_id);
-    $.db.query('UPDATE users SET ' +
-      updateKeys.join(',') + ' WHERE id=? LIMIT 1',
-      updateValues,
-      function(error, results) {
-        if(error) {
-          return$.error(error);
-        } else {
-          $.db.query('SELECT * FROM users WHERE id=? LIMIT 1', [user_id], 
-            function(error, results) {
-              if(error) {
-                return $.error(error);
-              } else {
-                return $.render(results[0] || 'false'); 
-              }
-          });
-        }
-      });
+    $.m.put_one($, user_id, ['name', 'email'], 'users');
   },
 
   // > DELETE /user/:int > json
@@ -123,22 +59,13 @@ module.exports = {
   // > curl -X DELETE http://localhost:8080/user/3
   // 
   // < 200 < json < {"id": 1}
-  // < 400 < json < {"error": 400, "message": "invalid parameters"} < user_id invalid or not found
-  // < 401 < json < {"error": 401, "message": "unauthorized"}
+  // < 400 < json < {"http": 400, "error": "invalid parameters"} < user_id invalid or not found
+  // < 500 < json < {"http": 500, "error": "unable to delete item"}  
+  // < 401 < json < {"http": 401, "error": "unauthorized"}
   'DELETE /user/:int':
   function($, user_id) {
-    if(!user_id) {
-      return $.error('invalid user_id');
-    }
+    $.m.delete_one($, user_id, 'users');
 
-    $.db.query('DELETE FROM users WHERE id=? LIMIT 1', [user_id], 
-      function(error, results) {
-        if(error) {
-          return $.error(error);
-        } else {
-          return $.render(user_id); 
-        }
-    });
   }
 
 };
