fd583d19286948847847364e4280c0bf42afe57f
mattes <matthias.kadenbach@gmail.com>
2013-01-30 23:12:29 +0100

    controllers updated

---
+++ api/controllers/event_groups.js
@@ -0,0 +1,88 @@
+module.exports = {
+
+  // > GET /event_groups > json
+  // > get all event_groups
+  //
+  // < 200 < json < [{"id": 1, "title": "Event Group", "text": "additional info about event group"}]
+  // < 401 < json < {"http": 401, "error": "unauthorized"}
+  // < 500 < json < {"http": 500, "error": "unable to fetch results"}
+  'GET /event_groups':
+  function($) {
+    $.m.get_all($, 'SELECT * FROM event_groups WHERE 1');
+  },
+
+  // > POST /event_group > json
+  // > create new event_group
+  // > {title": "Event Group", "text": "additional info about event group"}
+  // 
+  // < 200 < json < {"id": 1}
+  // < 500 < json < {"http": 500, "error": "unable to create new item"}
+  // < 401 < json < {"http": 401, "error": "unauthorized"}
+  'POST /event_group':
+  function($) {
+    $.m.post_one($, 'INSERT INTO event_groups SET title=?, text=?', 
+      [$.params.title, $.params.text]);
+  },
+
+  // > GET /event_group/:int > json
+  // > get user details about one event_group
+  // 
+  // < 200 < json < {"id": 1, title": "Event Group", "text": "additional info about event group"}
+  // < 400 < json < {"http": 400, "error": "invalid parameters"}
+  // < 500 < json < {"http": 500, "error": "unable to fetch result"}
+  // < 401 < json < {"http": 401, "error": "unauthorized"}
+  'GET /event_group/:int':
+  function($, event_group_id) {
+    $.m.get_one($, [event_group_id], 'SELECT id, title, text FROM event_groups WHERE id=? LIMIT 1');
+  },
+
+  // > PUT /event_group/:int > json
+  // > updates a event_group
+  // > {title": "Event Group", "text": "additional info about event group"}
+  // 
+  // < 200 < json < {"id": 1}
+  // < 400 < json < {"http": 400, "error": "invalid parameters"}
+  // < 500 < json < {"http": 500, "error": "unable to update item"}
+  // < 401 < json < {"http": 401, "error": "unauthorized"}
+  'PUT /event_group/:int':
+  function($, event_group_id) {
+    $.m.put_one($, [event_group_id], ['title', 'text'], 'event_groups', 'id=?');
+  },
+
+  // > DELETE /event_group/:int > json
+  // > delete one event_group
+  // 
+  // < 200 < json < {"id": 1}
+  // < 400 < json < {"http": 400, "error": "invalid parameters"}
+  // < 500 < json < {"http": 500, "error": "unable to delete item"}  
+  // < 401 < json < {"http": 401, "error": "unauthorized"}
+  'DELETE /event_group/:int':
+  function($, event_group_id) {
+    $.m.delete_one($, event_group_id, 'event_groups', 'id=?');
+  },
+
+  // > GET /event_group/:int/events > json
+  // > get all events for event_groups
+  //
+  // < 200 < json < [{"id": 1, "event_group_id": 1, "event_group": {event_group}, "created_by_user_id": 1, "created_by_user": {user}, "utc_timestamp": 0, "duration": 0}]
+  // < 401 < json < {"http": 401, "error": "unauthorized"}
+  // < 500 < json < {"http": 500, "error": "unable to fetch results"}
+  'GET /event_group/:int/events':
+  function($, event_group_id) {
+    $.m.get_all($, 'SELECT * FROM events WHERE event_group_id=? LIMIT 100', [event_group_id], 
+      {"event_group": 'SELECT id, title, text FROM event_groups WHERE id=?',
+       "created_by_user": 'SELECT id, name, email FROM users WHERE id=?'});
+  },
+
+  // > GET /event_group/:int/users > json
+  // > get all users for event_groups
+  //
+  // < 200 < json < [{"id": 1, "name": "Peter", "email": "peter@example.com"}]
+  // < 401 < json < {"http": 401, "error": "unauthorized"}
+  // < 500 < json < {"http": 500, "error": "unable to fetch results"}
+  'GET /event_group/:int/users':
+  function($, event_group_id) {
+    $.m.get_all($, 'SELECT users.id, users.name, users.email FROM users INNER JOIN user_has_event_groups ON user_has_event_groups.user_id = users.id WHERE user_has_event_groups.event_group_id=? LIMIT 100', [event_group_id]);
+  }
+
+};
---
+++ api/controllers/events.js
@@ -0,0 +1,165 @@
+module.exports = {
+
+
+  // > GET /events > json
+  // > get all events
+  //
+  // < 200 < json < [{"id": 1, "event_group_id": 1, "event_group": {event_group}, "created_by_user_id": 1, "created_by_user": {user}, "utc_timestamp": 0, "duration": 0}]
+  // < 401 < json < {"http": 401, "error": "unauthorized"}
+  // < 500 < json < {"http": 500, "error": "unable to fetch results"}
+  'GET /events':
+  function($) {
+    $.m.get_all($, 'SELECT * FROM events WHERE 1', [], 
+      {"event_group": 'SELECT id, title, text FROM event_groups WHERE id=?',
+       "created_by_user": 'SELECT id, name, email FROM users WHERE id=?'});
+  },
+
+  // > POST /event > json
+  // > create new event
+  // > {"event_group_id": 1, "created_by_user_id": 1, "utc_timestamp": 0, "duration": 0}
+  // 
+  // < 200 < json < {"id": 1}
+  // < 500 < json < {"http": 500, "error": "unable to create new item"}
+  // < 401 < json < {"http": 401, "error": "unauthorized"}
+  'POST /event':
+  function($) {
+    $.m.post_one($, 'INSERT INTO events SET event_group_id=?, created_by_user_id=?, utc_timestamp=?, duration=?', 
+      [$.params.event_group_id, $.params.created_by_user_id, $.params.utc_timestamp, $.params.duration]);
+  },
+
+  // > GET /event/:int > json
+  // > get details about one event
+  // 
+  // < 200 < json < {"id": 1, "event_group_id": 1, "event_group": {event_group}, "created_by_user_id": 1, "created_by_user": {user}, "utc_timestamp": 0, "duration": 0}
+  // < 400 < json < {"http": 400, "error": "invalid parameters"}
+  // < 500 < json < {"http": 500, "error": "unable to fetch result"}
+  // < 401 < json < {"http": 401, "error": "unauthorized"}
+  'GET /event/:int':
+  function($, event_id) {
+    $.m.get_one($, [event_id], 'SELECT * FROM events WHERE id=? LIMIT 1', 
+      {"event_group": 'SELECT id, title, text FROM event_groups WHERE id=?',
+       "created_by_user": 'SELECT id, name, email FROM users WHERE id=?'});
+  },
+
+  // > PUT /event/:int > json
+  // > updates a event
+  // > {"event_group_id": 1, "created_by_user_id": 1, "utc_timestamp": 0, "duration": 0}
+  // 
+  // < 200 < json < {"id": 1}
+  // < 400 < json < {"http": 400, "error": "invalid parameters"}
+  // < 500 < json < {"http": 500, "error": "unable to update item"}
+  // < 401 < json < {"http": 401, "error": "unauthorized"}
+  'PUT /event/:int':
+  function($, event_id) {
+    $.m.put_one($, [event_id], ['event_group_id', 'created_by_user_id', 'utc_timestamp', 'duration'], 'events', 'id=?');
+  },
+
+  // > DELETE /event/:int > json
+  // > delete one event
+  // 
+  // < 200 < json < {"id": 1}
+  // < 400 < json < {"http": 400, "error": "invalid parameters"} < user_id invalid or not found
+  // < 500 < json < {"http": 500, "error": "unable to delete item"}  
+  // < 401 < json < {"http": 401, "error": "unauthorized"}
+  'DELETE /event/:int':
+  function($, event_id) {
+    $.m.delete_one($, event_id, 'events', 'id=?');
+  },
+
+
+  // > GET /event/:int/fields > json
+  // > get fields for event
+  // 
+  // < 200 < json < {"id": "key", "value": "value for key"}
+  // < 400 < json < {"http": 400, "error": "invalid parameters"}
+  // < 500 < json < {"http": 500, "error": "unable to fetch result"}
+  // < 401 < json < {"http": 401, "error": "unauthorized"}
+  'GET /event/:int/fields':
+  function($, event_id) {
+    $.m.get_one($, [event_id], 'SELECT id, value FROM event_fields WHERE id=? LIMIT 1');
+  },
+
+
+  // > POST /event/:int/field > json
+  // > create new field for event
+  // > {"id": "key", "value": "value for key"}
+  // 
+  // < 200 < json < {"id": "key"}
+  // < 500 < json < {"http": 500, "error": "unable to create new item"}
+  // < 401 < json < {"http": 401, "error": "unauthorized"}
+  'POST /event/:int/field':
+  function($, event_id) {
+    $.m.post_one($, 'INSERT INTO event_fields SET event_id=?, id=?, value=?', 
+      [event_id, $.params.id, $.params.value]);
+  },
+
+  // > GET /event/:int/field/:string > json
+  // > get field for event 
+  // 
+  // < 200 < json < {"id": "key", "value": "value for key"}
+  // < 400 < json < {"http": 400, "error": "invalid parameters"}
+  // < 500 < json < {"http": 500, "error": "unable to fetch result"}
+  // < 401 < json < {"http": 401, "error": "unauthorized"}
+  'GET /event/:int/field/:string':
+  function($, event_id, id) {
+    $.m.get_one($, [event_id, id], 'SELECT id, value FROM event_fields WHERE event_id=? AND id=? LIMIT 1');
+  },  
+
+  // > PUT /event/:int/field/:string > json
+  // > updates a field for an event
+  // > {"id": "key", "value": "value for key"}
+  // 
+  // < 200 < json < {"id": "key"}
+  // < 400 < json < {"http": 400, "error": "invalid parameters"}
+  // < 500 < json < {"http": 500, "error": "unable to update item"}
+  // < 401 < json < {"http": 401, "error": "unauthorized"}
+  'PUT /event/:int/field/:string':
+  function($, event_id, id) {
+    $.m.put_one($, [event_id, id], ['id', 'value'], 'event_fields', 'event_id=? AND id=?');
+  },
+
+
+  // > DELETE /event/:int/field/:string > json
+  // > delete one field for an event
+  // 
+  // < 200 < json < {"id": "key"}
+  // < 400 < json < {"http": 400, "error": "invalid parameters"} < user_id invalid or not found
+  // < 500 < json < {"http": 500, "error": "unable to delete item"}  
+  // < 401 < json < {"http": 401, "error": "unauthorized"}
+  'DELETE /event/:int/field/:string':
+  function($, event_id, id) {
+    $.m.delete_one($, [event_id, id], 'event_fields', 'event_id=? AND id=?');
+  },
+
+
+  // > GET /events/type/:string > json
+  // > get events with type 
+  // 
+  // < 200 < json < {"id": 1, "event_group_id": 1, "event_group": {event_group}, "created_by_user_id": 1, "created_by_user": {user}, "utc_timestamp": 0, "duration": 0}
+  // < 400 < json < {"http": 400, "error": "invalid parameters"}
+  // < 500 < json < {"http": 500, "error": "unable to fetch result"}
+  // < 401 < json < {"http": 401, "error": "unauthorized"}
+  'GET /events/type/:string':
+  function($, type) {
+    $.m.get_all($, 'SELECT events.* FROM events INNER JOIN event_fields ON event_fields.event_id=events.id WHERE event_fields.id=? LIMIT 100', [type], 
+      {"event_group": 'SELECT id, title, text FROM event_groups WHERE id=?',
+       "created_by_user": 'SELECT id, name, email FROM users WHERE id=?'});
+  },
+
+  // > GET /events/between/:string/and/:string > json
+  // > get events between A and B 
+  // 
+  // < 200 < json < {"id": 1, "event_group_id": 1, "event_group": {event_group}, "created_by_user_id": 1, "created_by_user": {user}, "utc_timestamp": 0, "duration": 0}
+  // < 400 < json < {"http": 400, "error": "invalid parameters"}
+  // < 500 < json < {"http": 500, "error": "unable to fetch result"}
+  // < 401 < json < {"http": 401, "error": "unauthorized"}
+  'GET /events/between/:string/and/:string':
+  function($, time1, time2) {
+    return $.error(500, 'not yet implemented');
+  }  
+
+
+
+
+
+};
