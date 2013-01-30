5e120584d146577e0ab630d151fed67a44367b61
mattes <matthias.kadenbach@gmail.com>
2013-01-30 02:04:46 +0100

    doc helper

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -1,119 +1,152 @@
 module.exports = {
 
-  // curl -X GET http://localhost:8080/users
+  // > GET /users > json
+  // > get all users
+  // > curl -X GET http://localhost:8080/users
+  //
+  // < 200 < json < [{"id": 1, "name": "Peter", "email": "peter@example.com", "is_admin": 0}]
+  // < 401 < json < {"error": 401, "message": "unauthorized"}
   'GET /users':
-  function(api) {
-    api.db.query('SELECT * FROM users WHERE 1', 
+  function($) {
+    $.db.query('SELECT * FROM users WHERE is_disabled=0', 
       function(error, results) {
         if(error) {
-          api.error(error);
+          $.error(error);
           return;
         } else {
-          api.render(results);
+          $.render(results);
         }
     });
   },
 
-  // curl -X POST --data "name=Matthias&email=matthias@example.com&password=12345" http://localhost:8080/user
+
+  // > POST /user > json
+  // > create new user
+  // > {"name": "Peter", "email": "peter@example.com", "password": "random"}
+  // > curl -X POST --data "name=Matthias&email=matthias@example.com&password=12345" http://localhost:8080/user
+  // 
+  // < 200 < json < {"id": 1}
+  // < 400 < json < {"error": 400, "message": "invalid parameters"}
+  // < 401 < json < {"error": 401, "message": "unauthorized"}
   'POST /user':
-  function(api) {
-    var apiAccessKey = 'some_random_string'; // @todo
-    api.db.query('INSERT INTO users SET ' +
-      'name=?, email=?, password=SHA1(?), api_access_key=?',
-      [api.params.name, api.params.email, api.params.password, apiAccessKey],
+  function($) {
+    var $AccessKey = 'some_random_string'; // @todo
+    $.db.query('INSERT INTO users SET ' +
+      'name=?, email=?, password=SHA1(?), $_access_key=?',
+      [$.params.name, $.params.email, $.params.password, $AccessKey],
       function(error, results) {
         if(error) {
-          api.error(error);
+          $.error(error);
           return;
         } else {
-          api.render(results.insertId);
+          $.render(results.insertId);
         }
       });
   },
 
-  // curl -X GET http://localhost:8080/user/1
+  // > GET /user/:int > json
+  // > get user details about one user
+  // > curl -X GET http://localhost:8080/user/1
+  // 
+  // < 200 < json < {"id": 1, "name": "Peter", "email": "peter@example.com"}
+  // < 400 < json < {"error": 400, "message": "invalid parameters"} < means user_id from url is missing or was not found
+  // < 401 < json < {"error": 401, "message": "unauthorized"}
   'GET /user/:int':
-  function(api, user_id) {
+  function($, user_id) {
     if(!user_id) {
-      api.error('invalid user_id');
+      $.error('invalid user_id');
       return;
     }
 
-    api.db.query('SELECT * FROM users WHERE id=? LIMIT 1', [user_id], 
+    $.db.query('SELECT * FROM users WHERE id=? LIMIT 1', [user_id], 
       function(error, results) {
         if(error) {
-          api.error(error);
+          $.error(error);
           return;
         } else {
-          api.render(results[0] || 'false'); 
+          $.render(results[0] || 'false'); 
         }
     });
   },
 
-  // curl -X PUT --data "email=newemail@example.com" http://localhost:8080/user/1
+
+  // > PUT /user/:int > json
+  // > updates a user
+  // > {"name": "Peter", "email": "peter@example.com"}
+  // > curl -X PUT --data "email=newemail@example.com" http://localhost:8080/user/1
+  // 
+  // < 200 < json < {"id": 1, "name": "Peter", "email": "peter@example.com"}
+  // < 400 < json < {"error": 400, "message": "invalid parameters"}
+  // < 401 < json < {"error": 401, "message": "unauthorized"}
   'PUT /user/:int':
-  function(api, user_id) {
+  function($, user_id) {
     if(!user_id) {
-      api.error('invalid user_id');
+      $.error('invalid user_id');
       return;
     }
 
-    // filter api.params
+    // filter $.params
     var allowFields = ['name', 'email'];
     var updateKeys = [];
     var updateValues = [];
-    var apiParamsKeys = Object.keys(api.params);
-    var apiParamsKeysLength = apiParamsKeys.length;
-    for(var i=0; i < apiParamsKeysLength; i++) {
-      if(~allowFields.indexOf(apiParamsKeys[i])) {
-        updateKeys.push(apiParamsKeys[i] + '=?');
-        updateValues.push(api.params[apiParamsKeys[i]]);
+    var $ParamsKeys = Object.keys($.params);
+    var $ParamsKeysLength = $ParamsKeys.length;
+    for(var i=0; i < $ParamsKeysLength; i++) {
+      if(~allowFields.indexOf($ParamsKeys[i])) {
+        updateKeys.push($ParamsKeys[i] + '=?');
+        updateValues.push($.params[$ParamsKeys[i]]);
       }
     }
 
     // anything to update?
     if(updateKeys.length == 0) {
-      api.render('false');
+      $.render('false');
       return; 
     }
 
     updateValues.push(user_id);
-    api.db.query('UPDATE users SET ' +
+    $.db.query('UPDATE users SET ' +
       updateKeys.join(',') + ' WHERE id=? LIMIT 1',
       updateValues,
       function(error, results) {
         if(error) {
-          api.error(error);
+          $.error(error);
           return;
         } else {
-          api.db.query('SELECT * FROM users WHERE id=? LIMIT 1', [user_id], 
+          $.db.query('SELECT * FROM users WHERE id=? LIMIT 1', [user_id], 
             function(error, results) {
               if(error) {
-                api.error(error);
+                $.error(error);
                 return;
               } else {
-                api.render(results[0] || 'false'); 
+                $.render(results[0] || 'false'); 
               }
           });
         }
       });
   },
 
-  // curl -X DELETE http://localhost:8080/user/3
+  // > DELETE /user/:int > json
+  // > delete one user
+  // > curl -X DELETE http://localhost:8080/user/3
+  // 
+  // < 200 < json < {"id": 1}
+  // < 400 < json < {"error": 400, "message": "invalid parameters"} < user_id invalid or not found
+  // < 401 < json < {"error": 401, "message": "unauthorized"}
   'DELETE /user/:int':
-  function(api, user_id) {
+  function($, user_id) {
     if(!user_id) {
-      api.error('invalid user_id');
+      $.error('invalid user_id');
       return;
     }
 
-    api.db.query('DELETE FROM users WHERE id=? LIMIT 1', [user_id], 
+    $.db.query('DELETE FROM users WHERE id=? LIMIT 1', [user_id], 
       function(error, results) {
         if(error) {
-          api.error(error);
+          $.error(error);
           return;
         } else {
-          api.render(user_id); 
+          $.render(user_id); 
         }
     });
   }
