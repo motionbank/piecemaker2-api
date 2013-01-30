43f0b886eec549adca6533bce4fd60ab4dd68f85
mattes <matthias.kadenbach@gmail.com>
2013-01-30 23:20:30 +0100

    controllers updated

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -68,15 +68,28 @@ module.exports = {
   },
 
 
-
+  // > GET /user/:int/events > json
+  // > get all events for user
+  //
+  // < 200 < json < [{"id": 1, "event_group_id": 1, "event_group": {event_group}, "created_by_user_id": 1, "created_by_user": {user}, "utc_timestamp": 0, "duration": 0}]
+  // < 401 < json < {"http": 401, "error": "unauthorized"}
+  // < 500 < json < {"http": 500, "error": "unable to fetch results"}
   'GET /user/:int/events':
   function($, user_id) {
+    $.m.get_all($, 'SELECT * FROM events WHERE created_by_user_id=? LIMIT 100', [user_id], 
+      {"event_group": 'SELECT id, title, text FROM event_groups WHERE id=?',
+       "created_by_user": 'SELECT id, name, email FROM users WHERE id=?'});
+  },
 
-  }
-
+  // > GET /user/:int/event_groups > json
+  // > get all event_groups for user
+  //
+  // < 200 < json < [{"id": 1, "title": "Event Group", "text": "additional info about event group"}]
+  // < 401 < json < {"http": 401, "error": "unauthorized"}
+  // < 500 < json < {"http": 500, "error": "unable to fetch results"}
   'GET /user/:int/event_groups':
   function($, user_id) {
-
+    $.m.get_all($, 'SELECT event_groups.* FROM event_groups INNER JOIN user_has_event_groups ON user_has_event_groups.event_group_id = event_groups.id WHERE user_has_event_groups.user_id=? LIMIT 100', [user_id]);
   }
 
 };
