ab0b73ad80ac0c01970b84059c11a3c533d1fe37
mattes <matthias.kadenbach@gmail.com>
2013-03-18 16:51:23 +0100

    system utc_timestamp return success

--- api2/spec/controllers/system.js
+++ api2/spec/controllers/system.js
@@ -6,16 +6,13 @@ describe('controllers/system.js', function(){
 
 
   describe('GET /system/utc_timestamp', function(){
-    it('gets unix timestamp with milliseconds');
-    /*
     it('get unix timestamp with milliseconds', function(done){
       request
         .get('/system/utc_timestamp.json')
         .expect('Content-Type', 'application/json')
-        .expect(200, { id: 601 })
+        .expect(200)
         .end(done);
     });
-    */
   });
 
 });
