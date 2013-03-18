8caac87f05992210ef0f34634ae18623d8292885
mattes <matthias.kadenbach@gmail.com>
2013-03-18 17:26:19 +0100

    readme updated

--- api/controllers/system.js
+++ api/controllers/system.js
@@ -1,1 +1,14 @@
-// @todo GET unix timestamp + ms
+var sequence = require('sequence');
+var async = require('async');
+var _ = require('underscore');
+
+module.exports = {
+
+  'GET /system/utc_timestamp':
+  // get unix timestamp with milliseconds
+  //  returns time
+  function($) {
+    return $.render(Date.now() / 1000);
+  }
+
+};
