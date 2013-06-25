c0bffb5d309c81e2aea6e1ce479075c1ced52dd7
mattes <matthias.kadenbach@gmail.com>
2013-06-25 17:56:40 +0200

    lots of files added

---
+++ installer/puppet/manifests/site.pp
@@ -0,0 +1,15 @@
+Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ] }
+
+# virtual resource
+@exec { 'sudo apt-get update':
+   tag => update
+}
+
+# realize resource. filter by "update" tag
+# and relate it to all Package resources
+Exec <| tag == update |> -> Package <| |>
+
+node default {
+    include apache
+}
+
