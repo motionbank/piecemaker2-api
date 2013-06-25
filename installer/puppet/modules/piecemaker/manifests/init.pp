b7cc95907ffca8352b759b7a23ffc7a46a93eb63
mattes <matthias.kadenbach@gmail.com>
2013-06-26 01:35:13 +0200

    working puppet modules

--- installer/puppet/modules/piecemaker/manifests/init.pp
+++ installer/puppet/modules/piecemaker/manifests/init.pp
@@ -1,9 +1,9 @@
 class piecemaker {
 
     # start api
-    # exec { "piecemaker.api.start":
-    #   command => "node /piecemaker/api/api.js --env development",
-    #   logoutput => "on_failure",
-    # }
-    # 
+    exec { "piecemaker.api.start":
+      cwd => '/piecemaker/api',
+      command => "forever start --watchDirectory . -a -l /piecemaker/api/logs/forever.log -o /piecemaker/api/logs/out.log -e /piecemaker/api/logs/err.log api.js --env development",
+      logoutput => "on_failure",
+    }
 }
