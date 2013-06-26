05c43deca267b7455b1fb6b2a1c55d196b38079e
mattes <matthias.kadenbach@gmail.com>
2013-06-26 19:49:25 +0200

    one line installer added

---
+++ installer/one-liner.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# This file is one-line install script for the piecemaker API.
+# For more information, please visit https://github.com/fjenett/piecemaker2
+# Use this file like this:
+# curl 
+
