eb68c78bc100f0797b01a810a22babf26a729b76
mattes <matthias.kadenbach@gmail.com>
2013-07-16 02:49:58 +0200

    specs updated

--- api2/config/piecemaker.rb
+++ api2/config/piecemaker.rb
@@ -1,26 +1,26 @@
 require 'rubygems'
 require "em-synchrony/mysql2"
 
+
 environment :production do
   ActiveRecord::Base.establish_connection(:adapter  => 'em_mysql2',
-                                          :database => 'piecemaker2_test',
+                                          :database => 'piecemaker2',
                                           :username => 'root',
-                                          :password => 'root',
+                                          :password => '',
                                           :host     => 'localhost',
                                           :pool     => 1)
 end
 
 environment :development do
   ActiveRecord::Base.establish_connection(:adapter  => 'em_mysql2',
-                                          :database => 'piecemaker2_test',
+                                          :database => 'piecemaker2',
                                           :username => 'root',
-                                          :password => 'root',
+                                          :password => '',
                                           :host     => 'localhost',
                                           :pool     => 1)
 end
 
 environment :test do
-  puts "hallo"
   ActiveRecord::Base.establish_connection(:adapter  => 'em_mysql2',
                                           :database => 'piecemaker2',
                                           :username => 'root',
