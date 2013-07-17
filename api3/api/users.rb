05f43cb3f1586c315adf0d0642098cf57ee43948
mattes <matthias.kadenbach@gmail.com>
2013-07-17 17:22:24 +0200

    api2 is not api3 ... haha

---
+++ api3/api/users.rb
@@ -0,0 +1,15 @@
+module Piecemaker
+  class Users < Grape::API
+
+    resource 'users' do
+      
+      desc "Returns all users."
+      get "/" do
+        User.all
+      end
+
+    end
+    
+
+  end
+end
