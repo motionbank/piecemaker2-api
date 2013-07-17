8347d171cb967321781b381ad7a8f1827b31af6f
mattes <matthias.kadenbach@gmail.com>
2013-07-17 19:15:38 +0200

    sequel with factories

--- api3/spec/api/users_spec.rb
+++ api3/spec/api/users_spec.rb
@@ -8,6 +8,7 @@ describe Piecemaker::API do
   end
 
   it "/api/v1/users returns all users" do
+    user1 = User.make
     get "/api/v1/users"
     last_response.status.should == 200
     last_response.body.should == { :id => 1, :name => "peter" }.to_json
