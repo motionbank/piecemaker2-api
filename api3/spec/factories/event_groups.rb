b4b69e90fd31e7fe1865dbe8872200faae270aae
mattes <matthias.kadenbach@gmail.com>
2013-07-18 00:11:09 +0200

    factories updated

---
+++ api3/spec/factories/event_groups.rb
@@ -0,0 +1,11 @@
+EventGroup.factory :groupa do
+  id 21
+  title "Group A"
+  text "Event Group A"
+end
+
+EventGroup.factory :groupb do
+  id 22
+  title "Group B"
+  text "Event Group B"
+end
---
+++ api3/spec/factories/user_has_event_groups.rb
@@ -0,0 +1,20 @@
+UserHasEventGroup.factory :peter_groupa do
+  user_id 1001 # peter
+  event_group_id 21
+end
+
+UserHasEventGroup.factory :peter_groupb do
+  user_id 1001 # peter
+  event_group_id 22
+end
+
+
+UserHasEventGroup.factory :peter_groupa do
+  user_id 1002 # pan
+  event_group_id 21
+end
+
+UserHasEventGroup.factory :peter_groupb do
+  user_id 1002 # pan
+  event_group_id 22
+end
