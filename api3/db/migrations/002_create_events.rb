2caf4093b7bf70ca3f2205aaff30cf1a2b5970ac
mattes <matthias.kadenbach@gmail.com>
2013-07-17 22:49:24 +0200

    constraint errors when rake db:reset, and rake db:create admin

--- api3/db/migrations/002_create_events.rb
+++ api3/db/migrations/002_create_events.rb
@@ -1,14 +1,15 @@
 Sequel.migration do
   up do
     create_table(:events) do
-      Integer :id
+      primary_key :id
+
       Integer :event_group_id, :null => false
       Integer :created_by_user_id
 
       BigDecimal :utc_timestamp, :size => [20, 6], :null => false
       BigDecimal :duration, :size => [11, 6], :null => true
       
-      primary_key [:id]
+      
       
     end
   end
