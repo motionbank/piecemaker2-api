9c5faeedc993a644e1e50430f1a7082d5c8f1f2c
mattes <matthias.kadenbach@gmail.com>
2013-07-18 03:30:37 +0200

    rake file updated with short prod|dev args

---
+++ api3/spec/models/user_has_event_group_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+
+describe UserHasEventGroup do
+
+  before(:all) do
+    truncate_db
+    
+    @pan = User.make :pan
+
+    @event_group = EventGroup.make :alpha
+
+    @event = Event.make :big, 
+      :event_group_id => @event_group.id,
+      :created_by_user_id => @pan.id
+    
+    @event_field = EventField.make :flag1,
+      :event_id => @event.id
+
+    @user_has_event_group = UserHasEventGroup.make :default,
+      :user_id => @pan.id,
+      :event_group_id => @event_group.id
+
+  end
+
+  it "has one user" do
+    @user_has_event_group.user.to_json.should == @user.to_json
+  end
+
+  it "has one event group" do
+    @user_has_event_group.event_group.to_json.should == @event_group.to_json
+  end
+
+
+end
+
