23e48aaf5fb302f993832099d0f2b24f60380ced
mattes <matthias.kadenbach@gmail.com>
2013-07-18 04:32:57 +0200

    specs templates for api files added

---
+++ api3/spec/api/event_spec.rb
@@ -0,0 +1,46 @@
+require 'spec_helper'
+
+describe "Piecemaker::API Event" do
+  include Rack::Test::Methods
+
+  def app
+    Piecemaker::API
+  end
+
+
+  it "POST /api/v1/event/:id/field creates a new field for event" do
+    # alias 
+    # POST AUTH /event/:event_id/field
+
+    # create new field for event
+    # Likes: token*, id*, value*
+    # Returns: {id}
+    raise
+  end
+
+  it "GET /api/v1/event/:id/field/:id returns field with id for event with id" do
+    # alias 
+    # get one field for event
+    # Likes: token*
+    # Returns: {value}
+    raise
+  end
+
+  it "PUT /api/v1/event/:id/field/:id updates field with id for event with id" do
+    # alias 
+    # updates a field for an event
+    # Likes: token*, value
+    # Returns: boolean
+    raise
+  end
+
+  it "DELETE /api/v1/event/:id/field/:id deletes field with id for event with id" do
+    # alias 
+    # delete one field for an event
+    # Likes: token*
+    # Returns: boolean
+    raise
+  end
+
+end
+
--- api3/spec/api/users_spec.rb
+++ api3/spec/api/users_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe Piecemaker::API do
+describe "Piecemaker::API User" do
   include Rack::Test::Methods
 
   def app
@@ -12,10 +12,34 @@ describe Piecemaker::API do
     @pan = User.make :pan
   end
 
-  it "/api/v1/users returns all users" do
+  it "GET /api/v1/users returns all users" do
     get "/api/v1/users.json"
     last_response.status.should == 200
     last_response.body.should == [@peter, @pan].to_json
+  end
+
+  it "POST /api/v1/user creates new user" do
+    raise
+  end
+
+  it "GET /api/v1/user/me returns currently logged in user" do
+    raise
+  end
+
+  it "GET /api/v1/user/:id returns user for id" do
+    raise
+  end
+
+  it "PUT /api/v1/user/:id updates user with id" do
+    raise
+  end
+
+  it "DELETE /api/v1/user/:id deletes user with id" do
+    raise
+  end
+
+  it "GET /api/v1/user/:id/event_groups returns all event_groups for user with id" do
+    raise
   end
 
 end
