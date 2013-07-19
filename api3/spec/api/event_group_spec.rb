113bf4e929cc91b8b2975e3d48d15f55decdf7d9
mattes <matthias.kadenbach@gmail.com>
2013-07-19 03:10:13 +0200

    moving tests from event_groups to events

--- api3/spec/api/event_group_spec.rb
+++ api3/spec/api/event_group_spec.rb
@@ -77,34 +77,6 @@ describe "Piecemaker::API EventGroup" do
     # Returns: [{id, event_group_id, event_group, created_by_user_id, created_by_user, utc_timestamp, duration}]
   end
 
-  it "GET /api/v1/group/:id/event/:id alias for /api/v1/event/:id" do
-    raise
-    # get details about one event
-    # Likes: token*
-    # Returns: {id, event_group_id, event_group, created_by_user_id, created_by_user, utc_timestamp, duration}
-  end
-
-  it "POST /api/v1/group/:id/event/:id creates new event and event_fields" do
-    raise
-    # create new event and create new event_fields for all non-events table fields
-    # Likes: token*, utc_timestamp, duration, ...
-    # Returns: {id}
-  end
-
-  it "PUT /api/v1/group/:id/event/:id updates an event with id" do
-    raise
-    # updates a event
-    # Likes: token*, utc_timestamp, duration
-    # Returns: boolean
-  end
-
-  it "DELETE /api/v1/group/:id/event/:id deletes event with id" do
-    raise
-    # delete one event
-    # Likes: token*
-    # Returns: boolean
-  end
-
   it "GET /api/v1/group/:id/event/by_type/:type returns events by type" do
     raise
     # get events with type
