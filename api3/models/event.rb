a80de9a642c97b53c2bc42626e42d322f8e134d2
mattes <matthias.kadenbach@gmail.com>
2013-07-19 03:43:18 +0200

    finished tests for event group .. well almost

--- api3/models/event.rb
+++ api3/models/event.rb
@@ -2,9 +2,9 @@ class Event < Sequel::Model(:events)
   
   set_primary_key :id
 
-  # many_to_one :user, :key => :created_by_user_id, :primary_key => :id
+  many_to_one :user, :key => :created_by_user_id, :primary_key => :id
 
-  # one_to_many :event_fields
-  # many_to_one :event_group, :key => :event_group_id, :primary_key => :id
+  one_to_many :event_fields
+  many_to_one :event_group, :key => :event_group_id, :primary_key => :id
 
 end
--- api3/models/event_groups.rb
+++ api3/models/event_groups.rb
@@ -2,7 +2,7 @@ class EventGroup < Sequel::Model(:event_groups)
   
   set_primary_key :id
 
-  # one_to_many :events
-  # many_to_many :users, :join_table => :user_has_event_groups
+  one_to_many :events
+  many_to_many :users, :join_table => :user_has_event_groups
   
 end
