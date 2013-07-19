41d8afe96cb4e3a535b5dc15fb386f8c312dfda5
mattes <matthias.kadenbach@gmail.com>
2013-07-19 19:21:34 +0200

    making the code nice.

--- api3/api/event_groups.rb
+++ api3/api/event_groups.rb
@@ -2,32 +2,48 @@ module Piecemaker
 
   class EventGroups < Grape::API
 
-    resource 'groups' do
+    #===========================================================================
+    resource 'groups' do #======================================================
+    #===========================================================================
 
-      # ----------------------------------------------------------
+
+      #_________________________________________________________________________
+      ##########################################################################
       desc "returns all event_groups for currently logged in user"
-      get "/" do
+      #-------------------------------------------------------------------------
+      get "/" do  #/api/v1/groups
+      #-------------------------------------------------------------------------
         @_user = authorize!
         # @todo acl!
         EventGroup.eager_graph(:users).where(:user_id => @_user.id)
       end
+  
     end
 
-    resource 'group' do
 
-      # ---------------------------------------------------
+    #===========================================================================
+    resource 'group' do #=======================================================
+    #===========================================================================
+      
+
+      #_________________________________________________________________________
+      ##########################################################################
       desc "creates and returns new event and event_fields"
+      #-------------------------------------------------------------------------
       params do
         requires :id, type: Integer, desc: "event group id"
         requires :utc_timestamp, type: Float, desc: "utc timestamp"
         optional :duration, type: Float, desc: "duration"
         optional :fields, type: Hash, desc: "optional fields to create for this event {'field1': 'value', ...}"
       end 
-      post "/:id/event" do
+      #-------------------------------------------------------------------------
+      post "/:id/event" do  #/api/v1/group/:id/event
+      #-------------------------------------------------------------------------
         @_user = authorize!
         # @todo acl!
 
-        @event_group = EventGroup.first(:id => params[:id]) || error!('Not found', 404)
+        @event_group = EventGroup.first(
+          :id => params[:id]) || error!('Not found', 404)
 
         @event = Event.create(
           :event_group_id     => @event_group.id,
@@ -48,14 +64,20 @@ module Piecemaker
         
         [@event, fields]
       end
+      
 
-      # --------------------------------------------------
+      #_________________________________________________________________________
+      ##########################################################################
       desc "create new event_group (together with user_has_event_groups record)"
+      #-------------------------------------------------------------------------
       params do
         requires :title, type: String, desc: "name of the group"
-        requires :text, type: String, desc: "some additional description" # @todo type: Text not String
+        requires :text, type: String, desc: "some additional description"
+          # @todo type: Text not String
       end 
-      post "/" do
+      #-------------------------------------------------------------------------
+      post "/" do  #/api/v1/group
+      #-------------------------------------------------------------------------
         @_user = authorize!
         # @todo acl!
 
@@ -69,26 +91,35 @@ module Piecemaker
 
         return @event_group
       end
+      
 
-      # --------------------------------------------------
+      #_________________________________________________________________________
+      ##########################################################################
       desc "returns event_group with id"
+      #-------------------------------------------------------------------------
       params do
         requires :id, type: Integer, desc: "event group id"
       end
-      get "/:id" do
+      #-------------------------------------------------------------------------
+      get "/:id" do  #/api/v1/group/:id
+      #-------------------------------------------------------------------------
         @_user = authorize!
         EventGroup.first(:id => params[:id]) || error!('Not found', 404)
       end
+      
 
-      # --------------------------------------------------
+      #_________________________________________________________________________
+      ##########################################################################
       desc "updates event_group with id"
+      #-------------------------------------------------------------------------
       params do
         requires :id, type: Integer, desc: "event group id"
         optional :name, type: String, desc: "name of the group"
         optional :title, type: String, desc: "some additional description"
-
       end
-      put "/:id" do
+      #-------------------------------------------------------------------------
+      put "/:id" do  #/api/v1/group/:id
+      #-------------------------------------------------------------------------
         @_user = authorize!
         @event_group = EventGroup.first(:id => params[:id])
         error!('Not found', 404) unless @event_group
@@ -97,48 +128,61 @@ module Piecemaker
 
         @event_group.save
       end
+      
 
-      # --------------------------------------------------
+      #_________________________________________________________________________
+      ##########################################################################
       desc "deletes event_group with id"
+      #-------------------------------------------------------------------------
       params do
         requires :id, type: Integer, desc: "event_group id"
       end
-      delete "/:id" do
+      #-------------------------------------------------------------------------
+      delete "/:id" do #/api/v1/group/:id
+      #-------------------------------------------------------------------------
         @_user = authorize!
         @event_group = EventGroup.first(:id => params[:id])
         error!('Not found', 404) unless @event_group
 
         @event_group.delete
       end
+      
 
-      # --------------------------------------------------
+      #_________________________________________________________________________
+      ##########################################################################
       desc "returns all events"
+      #-------------------------------------------------------------------------
       params do
         requires :id, type: Integer, desc: "event group id"
       end
-      get "/:id/events" do
+      #-------------------------------------------------------------------------
+      get "/:id/events" do  #/api/v1/group/:id/events
+      #-------------------------------------------------------------------------
         @_user = authorize!
         @event_group = EventGroup.first(:id => params[:id])
         error!('Not found', 404) unless @event_group
 
         Event.where(:event_group_id => @event_group.id)
       end
+      
 
-      # --------------------------------------------------
+      #_________________________________________________________________________
+      ##########################################################################
       desc "returns all users for event_group with id"
+      #-------------------------------------------------------------------------
       params do
         requires :id, type: Integer, desc: "event group id"
       end
-      get "/:id/users" do
+      #-------------------------------------------------------------------------
+      get "/:id/users" do  #/api/v1/group/:id/users
+      #-------------------------------------------------------------------------
         @_user = authorize!
         @event_group = EventGroup.first(:id => params[:id])
         error!('Not found', 404) unless @event_group
 
         @event_group.users
-
       end
 
     end
-
   end
 end
