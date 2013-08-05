6b9432c3ce139840a85fd8e2c84c5b18ad420544
Florian Jenett <fjenett@gmail.com>
2013-08-05 23:13:57 +0200

    resolving api dir into root dir

--- api/models/event_field.rb
+++
@@ -1,8 +0,0 @@
-class EventField < Sequel::Model(:event_fields)
-  
-  set_primary_key [:event_id, :id]
-  set_dataset dataset.order(:id)
-
-  one_to_one :event, :key => :id, :primary_key => :event_id
-  
-end
--- api/spec/factories/user_has_event_groups.rb
+++
@@ -1,2 +0,0 @@
-UserHasEventGroup.factory do
-end
