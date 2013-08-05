cec36fcce78d39f6c9ee5c2f101f201e38188cfb
mattes <matthias.kadenbach@gmail.com>
2013-08-05 17:45:00 +0200

    code cleanup

--- api3/models/user.rb
+++ api3/models/user.rb
@@ -1,6 +1,3 @@
-# http://sequel.rubyforge.org/rdoc/classes/Sequel/Model/Associations/ClassMethods.html
-# http://sequel.rubyforge.org/rdoc/files/doc/association_basics_rdoc.html
-
 class User < Sequel::Model(:users)
   set_primary_key :id
   set_dataset dataset.order(:name)
@@ -11,8 +8,3 @@ class User < Sequel::Model(:users)
     :join_table => :user_has_event_groups
   
 end
-
-# user_id, can,  in
-# 1,       read, event_groups(id)
-# 1        read, event_groups
-# 1,       write, event_groups()
