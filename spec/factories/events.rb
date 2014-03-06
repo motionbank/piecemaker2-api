b870255159e797f24039d401df0038d357bc5a14
Matthias Kadenbach <matthias.kadenbach@gmail.com>
2014-03-06 22:10:41 +0100

    cleanup

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -1,11 +1,11 @@
 Event.factory :small do
-  utc_timestamp 5.0 # @todo use real values
+  utc_timestamp 5.0
   duration 10.0
   type "small"
 end
 
 Event.factory :big do
-  utc_timestamp 20.0 # @todo use real values
+  utc_timestamp 20.0
   duration 7.0
   type "big"
 end
