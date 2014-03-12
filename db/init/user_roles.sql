b9d9d0974bbf74055b1b9e1b7f6ececa5b4e27ea
Matthias Kadenbach <matthias.kadenbach@gmail.com>
2014-03-12 21:03:38 +0100

    fixed db schema #93

--- db/init/user_roles.sql
+++ db/init/user_roles.sql
@@ -1,4 +1,4 @@
-id,inherit_from_db,description
+id,inherit_from_id,description
 admin,user,the force is with him
 group_admin,user,the admin of a group
 user,guest,to be defined
--- db/migrations/018_allow_event_groups_created_by_user_id_to_be_null.rb
+++ db/migrations/018_allow_event_groups_created_by_user_id_to_be_null.rb
@@ -1,13 +1,17 @@
 Sequel.migration do
   up do
-    drop_foreign_key([:created_by_user_id])
-    add_foreign_key([:created_by_user_id], :users, :key => :id, 
-      :on_delete => :set_null, :on_update => :cascade)
+    alter_table(:event_groups) do
+      drop_foreign_key([:created_by_user_id])
+      add_foreign_key([:created_by_user_id], :users, :key => :id, 
+        :on_delete => :set_null, :on_update => :cascade)
+    end
   end
 
   down do
-    drop_foreign_key([:created_by_user_id])
-    add_foreign_key([:created_by_user_id], :users, :key => :id, 
-      :on_delete => :restrict, :on_update => :cascade)
+    alter_table(:event_groups) do
+      drop_foreign_key([:created_by_user_id])
+      add_foreign_key([:created_by_user_id], :users, :key => :id, 
+        :on_delete => :restrict, :on_update => :cascade)
+    end
   end
 end
