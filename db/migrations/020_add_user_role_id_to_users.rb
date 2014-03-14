a00c51768ec7026a51f0bd4bd2cfe7a2ad3eec25
Matthias Kadenbach <matthias.kadenbach@gmail.com>
2014-03-14 13:40:41 +0100

    added :default => user

--- db/migrations/020_add_user_role_id_to_users.rb
+++ db/migrations/020_add_user_role_id_to_users.rb
@@ -1,7 +1,7 @@
 Sequel.migration do
   up do
     alter_table(:users) do
-      add_column :user_role_id, String, :size => 50, :null => false
+      add_column :user_role_id, String, :size => 50, :null => false, :default => "user"
       add_foreign_key([:user_role_id], :user_roles, :key => :id, 
         :on_delete => :restrict, :on_update => :cascade)
     end    
