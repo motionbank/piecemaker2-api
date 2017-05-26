f1db793ce357be918f887379470ea8847f8ea147
Anton Koch <anton.koch@hs-mainz.de>
2017-05-26 17:36:02 +0200

    add missing roles to preset sql file

--- db/init/user_roles.sql
+++ db/init/user_roles.sql
@@ -2,4 +2,6 @@ id,inherit_from_id,description
 super_admin,admin,unicorn alike: can do everything
 admin,user,has some power
 group_admin,user,the admin of a group
+group_user,user,Read only access to a group
+annotator,group_user,Read and write access to group items
 user,,default user
