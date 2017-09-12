eccb037b118646fdeb1ae50322cf2bac6fe1bc43
Florian Jenett <fjenett@gmail.com>
2017-09-12 20:28:45 +0200

    git merge killed the permissions … had to fix them

--- db/init/role_permissions.sql
+++ db/init/role_permissions.sql
@@ -1,21 +1,31 @@
 user_role_id,action,allowed
-group_admin,get_event_group,t
-group_admin,delete_event_group,t
-group_admin,update_event_group,t
-group_admin,get_users_for_event_group,t
+annotator,create_new_event,t
+annotator,delete_event,t
+annotator,update_event,t
 group_admin,add_user_to_event_group,t
-group_admin,update_users_attributes_for_event_group,t
-group_admin,delete_user_in_event_group,t
-group_admin,get_events,t
 group_admin,create_new_event,t
-group_admin,update_event,t
 group_admin,delete_event,t
+group_admin,delete_event_group,t
+group_admin,delete_user_in_event_group,t
+group_admin,get_event_group,t
+group_admin,get_events,t
+group_admin,get_users_for_event_group,t
+group_admin,update_event,t
+group_admin,update_event_group,t
+group_admin,update_users_attributes_for_event_group,t
+group_user,get_event_group,t
+group_user,get_events,t
+group_user,get_my_event_groups,t
 super_admin,add_user_to_event_group,t
 super_admin,create_new_event,t
 super_admin,create_new_event_group,t
+super_admin,create_new_permission,t
+super_admin,create_new_role,t
 super_admin,create_new_user,t
 super_admin,delete_event,t
 super_admin,delete_event_group,t
+super_admin,delete_permission,t
+super_admin,delete_role,t
 super_admin,delete_user,t
 super_admin,delete_user_in_event_group,t
 super_admin,get_all_event_groups,t
@@ -23,65 +33,15 @@ super_admin,get_event_group,t
 super_admin,get_event_groups_for_user,t
 super_admin,get_events,t
 super_admin,get_my_event_groups,t
+super_admin,get_permission,t
+super_admin,get_role,t
 super_admin,get_roles,t
 super_admin,get_user,t
 super_admin,get_users,t
 super_admin,get_users_for_event_group,t
 super_admin,update_event,t
 super_admin,update_event_group,t
+super_admin,update_permission,t
+super_admin,update_role,t
 super_admin,update_user,t
 super_admin,update_users_attributes_for_event_group,t
-super_admin,get_role,t
-super_admin,create_new_role,t
-super_admin,update_role,t
-super_admin,delete_role,t
-super_admin,create_new_permission,t
-super_admin,get_permission,t
-super_admin,update_permission,t
-super_admin,delete_permission,t
-group_admin	get_event_group	t
-group_admin	delete_event_group	t
-group_admin	update_event_group	t
-group_admin	get_users_for_event_group	t
-group_admin	add_user_to_event_group	t
-group_admin	update_users_attributes_for_event_group	t
-group_admin	delete_user_in_event_group	t
-group_admin	get_events	t
-group_admin	create_new_event	t
-group_admin	update_event	t
-group_admin	delete_event	t
-super_admin	add_user_to_event_group	t
-super_admin	create_new_event	t
-super_admin	create_new_event_group	t
-super_admin	create_new_user	t
-super_admin	delete_event	t
-super_admin	delete_event_group	t
-super_admin	delete_user	t
-super_admin	delete_user_in_event_group	t
-super_admin	get_all_event_groups	t
-super_admin	get_event_group	t
-super_admin	get_event_groups_for_user	t
-super_admin	get_events	t
-super_admin	get_my_event_groups	t
-super_admin	get_roles	t
-super_admin	get_user	t
-super_admin	get_users	t
-super_admin	get_users_for_event_group	t
-super_admin	update_event	t
-super_admin	update_event_group	t
-super_admin	update_user	t
-super_admin	update_users_attributes_for_event_group	t
-super_admin	get_role	t
-super_admin	create_new_role	t
-super_admin	update_role	t
-super_admin	delete_role	t
-super_admin	create_new_permission	t
-super_admin	get_permission	t
-super_admin	update_permission	t
-super_admin	delete_permission	t
-group_user	get_event_group	t
-group_user	get_my_event_groups	t
-group_user	get_events	t
-annotator	create_new_event	t
-annotator	delete_event	t
-annotator	update_event	t
