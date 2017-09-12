682ba4a5bcab9f759c732557461be9ead75a20e5
Florian Jenett <fjenett@gmail.com>
2017-09-12 17:15:19 +0200

    updated user permissions and roles

--- db/init/user_roles.sql
+++ db/init/user_roles.sql
@@ -1,5 +1,7 @@
 id,inherit_from_id,description
-super_admin,admin,unicorn alike: can do everything
-admin,user,has some power
-group_admin,user,the admin of a group
+super_admin,admin,A unicorn: can do everything
+admin,user,Has some power
+group_admin,user,The admin of a group
+annotator,group_user,Read and write access to group items
+group_user,user,Read only access to a group
 user,,default user
